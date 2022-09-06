095d4e3493b13bb68d760fe0aa383d03a7b32d5a
Mitchell Foote <mcfoote321@gmail.com>
2022-09-06 03:38:37 -0400

    continue fleshing out js

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,6 +7,69 @@ var button = document.querySelector('searchButton');
 var weatherConditions = [];
 var dataStore = JSON.parse(localStorage.getItem('cities')) || [];
 
+var clearElement = function(element) {
+
+    element.innerHTML = '';
+
+};
+
+var cityLoad = function() {
+
+    clearElement(history);
+
+    if(dataStore) {
+
+        var ulElement = document.createElement('ul');
+        ulElement.classListadd('w-100');
+        ulElement.classListadd('list-unstyled');
+
+        for(let i = 0; i < dataStore.length; i++) {
+
+            var liElement = document.createElement('li');
+            liElement.innerHTML = "<button type='button' class='list-group-item list-group-item-action' attr='"+dataStore[i]+"'>" + dataStore[i] + "</button>";
+            ulElement.appendChild(liElement);
+
+        }
+
+        history.appendChild(ulElement);
+
+    }
+
+};
+
+var citySave = function(city){
+
+    var flag = false
+    if(dataStore){
+
+        for(var i = 0; i < dataStore.length; i++){
+
+            if(dataStore[i] === city){
+
+                flag = true;
+                
+            }
+
+        }
+
+        if(flag){
+
+            displayAlertMessage("The City: "+city+" already exists")
+
+        }
+    }
+
+    if(!flag){
+
+        dataStore.push(city);
+        localStorage.setItem("cities",JSON.stringify(dataStore));
+
+    }
+    
+    cityLoad();
+
+}
+
 var search = function(event) {
 
     event.preventDefault();
